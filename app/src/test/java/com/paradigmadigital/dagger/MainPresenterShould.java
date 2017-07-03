84c197e47366fe5233f4f24b5e346d1a3f890000
Miguel Sesma <miguel@sesma.eu>
2017-07-03 10:42:37 +0200

    Add unit tests

---
+++ app/src/test/java/com/paradigmadigital/dagger/MainPresenterShould.java
@@ -0,0 +1,47 @@
+package com.paradigmadigital.dagger;
+
+import com.paradigmadigital.dagger.ui.IAppCollaborator;
+import com.paradigmadigital.dagger.ui.Navigator;
+import com.paradigmadigital.dagger.ui.main.MainPresenter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MainPresenterShould {
+
+    @Mock
+    Navigator navigator;
+    @Mock
+    IAppCollaborator collaborator;
+
+    private MainPresenter presenter;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        presenter = new MainPresenter(navigator, collaborator);
+    }
+
+    @Test
+    public void getVersionFromCollaboratorWhengetCollaboratorVersion() {
+        when(collaborator.getCollaboratorVersion()).thenReturn("version");
+
+        String version = presenter.getCollaboratorVersion();
+
+        assertThat(version).isEqualTo("version");
+    }
+
+    @Test
+    public void navigateToDetailOnClick() {
+
+        presenter.onClick();
+
+        verify(navigator).navigateToDetail();
+    }
+}
